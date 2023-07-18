1dfd85d394977388239e963cb011aa7e60e85b21
Kirill <04kirill20@gmail.com>
2023-07-18 19:17:04 +1000

    initiall commit

---
+++ 5.py
@@ -0,0 +1,9 @@
+score=int(input("Введите кол-во очкоа команды за игру: "))
+if score==3:
+    print("Выигрыш")
+elif score==1:
+    print("Ничья")
+elif score==0:
+    print("Проигрышь")
+else:
+    print("Некорректное кол-во очков")
