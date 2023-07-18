82ba298320e225b201b286b027f34fc2261327b8
Kirill <04kirill20@gmail.com>
2023-07-18 19:23:04 +1000

    initiall commit

---
+++ 2.py
@@ -0,0 +1,13 @@
+TV=["Молодёжка", "Игра престолов", "Импровизация", "Comydy"]
+print("Текущий список передач: ")
+for show in TV:
+    print(show)
+
+new_shows=input('\n'"Введите название новой передачи: ")
+position=int(input("Введите позицию,на которую нужно вставить новую передачу (от1 до 5): "))
+
+TV.insert(position-1, new_shows)
+
+print("Новый список передач: ")
+for show in TV:
+    print(show)
