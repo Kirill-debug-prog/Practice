a03ce7b42292f084467a2329a2a10ee7c4033052
Kirill <04kirill20@gmail.com>
2023-07-18 19:23:37 +1000

    initiall commit

---
+++ 6.py
@@ -0,0 +1,10 @@
+distance = int(input("Сколько километров хотите проехать на автомобиле: "))
+consumption = int(input("Сколько литров топлива расходует автомобиль на 100 км: "))
+tank_capacity = int(input("Сколько литров топлива в вашем баке: "))
+
+fuel_needed = (distance/100)*consumption
+
+if fuel_needed<=tank_capacity:
+    print("Вы проедете желаемое расстояние")
+else:
+    print("У вс недостаточно топлива для поездки на данное расстояние")
