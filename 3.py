dcec821b8f2b3152dfd7ada34516f881a065913e
Kirill <04kirill20@gmail.com>
2023-07-18 19:16:42 +1000

    initiall commit

---
+++ 3.py
@@ -0,0 +1,24 @@
+num1=int(input("Введите первое число: "))
+num2=int(input("Введите второе число: "))
+num3=int(input("Введите третье число: "))
+
+if num1>=num2 and num1>=num3:
+    print(num1, "является наибольшим")
+    if num2>=num3:
+        print("Числа по убыванию", num1, num2, num3)
+    else:
+        print("Числа по убыванию", num1, num3, num2)
+
+elif num2>=num1 and num2>=num3:
+    print(num2, "является наибольшим")
+    if num1>=num3:
+        print("Числа по убыванию", num2, num1, num3)
+    else:
+        print("Числа по убыванию", num2, num3, num1)
+
+else:
+    print(num3, "является наибольшим")
+    if num1>=num3:
+        print("Числа по убыванию", num3, num1, num2)
+    else:
+        print("Числа по убыванию", num3, num2, num1)
