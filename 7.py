91e4022db2631b2ab74d5f0414a8786cb657a89d
Kirill <04kirill20@gmail.com>
2023-07-18 19:17:26 +1000

    initiall commit

---
+++ 7.py
@@ -0,0 +1,10 @@
+string=input("Вевиде строку: ")
+new_string=""
+for char in string:
+    if char.islower():
+        new_string+=char.upper()
+    elif char.isupper():
+        new_string+=char.lower()
+    else:
+        new_string+=char
+print(new_string)
