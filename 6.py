29433bd51403d5e7cc34fafc781864c025da60a2
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 16:25:14 +1000

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,8 +1,9 @@
 distance = int(input("Сколько километров хотите проехать на автомобиле: "))
-consumption = int(input("Сколько литров топлива расходует автомобиль на 100 км: "))
+consumption = int(input("Сколько литров топлива "
+                        "расходует автомобиль на 100 км: "))
 tank_capacity = int(input("Сколько литров топлива в вашем баке: "))
 
-fuel_needed = (distance/100) * consumption
+fuel_needed = (distance / 100) * consumption
 
 if fuel_needed <= tank_capacity:
     print("Вы проедете желаемое расстояние")
