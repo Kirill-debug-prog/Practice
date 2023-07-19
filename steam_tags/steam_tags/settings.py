c5ec2433c218a2bd2c46e44c3de3c6c860e53067
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 15:43:24 +1000

    Update settings.py

--- steam_tags/steam_tags/settings.py
+++ steam_tags/steam_tags/settings.py
@@ -1,10 +1,7 @@
 BOT_NAME = "steam_tags"
-
 SPIDER_MODULES = ["steam_tags.spiders"]
 NEWSPIDER_MODULE = "steam_tags.spiders"
-
 ROBOTSTXT_OBEY = True
-
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
