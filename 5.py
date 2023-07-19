1406cf330652e3215cd9fe1a9a731a8614f09598
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 14:52:16 +1000

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,9 +1,9 @@
-score=int(input("Введите кол-во очкоа команды за игру: "))
-if score==3:
+score = int(input("Введите кол-во очкоа команды за игру: "))
+if score == 3:
     print("Выигрыш")
-elif score==1:
+elif score == 1:
     print("Ничья")
-elif score==0:
+elif score == 0:
     print("Проигрышь")
 else:
     print("Некорректное кол-во очков")
