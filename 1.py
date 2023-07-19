02fc2455a42f4d872604f569279a73d24993f1ec
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 15:21:58 +1000

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,14 +1,12 @@
 import csv
 
-data = [
-    ["Книга", "Автор", "Год выпуска"],
+data = [["Книга", "Автор", "Год выпуска"],
     ["Tarzan", "Edgar Burroughs", "1990"],
     ["The Hobbit", "John R.R, Tolkien", "1937"],
-    ["Harry Potter and the Philosopher's Stone", 
+    ["Harry Potter and the Philosopher's Stone",
      "Roman, J.K. Rowling", "1997"],
-    ["Harry Potter and the Deathly Hallows Part 1", 
-     "Joanne Rowling", "2007"]
-]
+    ["Harry Potter and the Deathly Hallows Part 1",
+     "Joanne Rowling", "2007"]]
 
 filename = "books.csv"
 with open(filename, mode="w", newline='') as file:
