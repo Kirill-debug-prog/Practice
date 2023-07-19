9b3d52deaa0576ca128cfece1804651e808971f6
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 16:03:59 +1000

    Update 1.py

--- 1.py
+++ 1.py
@@ -27,8 +27,8 @@ try:
         else:
             # Выводим информацию о топ 10 песен
             for i in range(10):
-                song_title = songs[i].find('a',
-                                           class_='track__title js-track-title').text.strip()
+                song_title = songs[i].find\
+                    ('a', class_='track__title js-track-title').text.strip()
                 print(f"{i + 1}. {song_title}")
     else:
         print("Ошибка при отправке запроса. "
