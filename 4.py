adba9ed1acbdf6878a482f53b21ce9b66038dcc7
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 16:24:01 +1000

    Update 4.py

--- 4.py
+++ 4.py
@@ -3,11 +3,11 @@ a = float(input("Введите коэффицент 'a': "))
 b = float(input("Введите коэффицент 'b': "))
 c = float(input("Введите коэффицент 'c': "))
 
-D= (b ** 2 - 4 * c * a)
+D = (b ** 2 - 4 * c * a)
 if D < 0:
     print("Корней нет")
 elif D == 0:
-    x = (- b) / (2 *  a)
+    x = (- b) / (2 * a)
     print(x)
 else:
     x1 = (- b + math.sqrt(D)) / (2 * a)
