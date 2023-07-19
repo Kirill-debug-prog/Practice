baf200449bd8067e6875e80218a8a35a1e8d1c7e
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 17:17:25 +1000

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,19 +1,19 @@
-def permutation(l):
-    if len(l) == 0:
+def permutation(List):
+    if len(List) == 0:
         return [[]]
-    elif len(l) == 1:
-        return [l]
+    elif len(List) == 1:
+        return [List]
     else:
         permutations = []
-        for i in range(len(l)):
-            rest = l[:i] + l[i + 1:]
+        for i in range(len(List)):
+            rest = List[:i] + List[i + 1:]
             for j in permutation(rest):
-                permutations.append([l[i]] + j)
+                permutations.append([List[i]] + j)
         return permutations
 
 
-l_1 = int(input("Введите стартовое число: "))
-l_2 = int(input("Введите конечное число: "))
-l = [x for x in range(l_1, l_2 + 1)]
-permutations = permutation(l)
+list_1 = int(input("Введите стартовое число: "))
+list_2 = int(input("Введите конечное число: "))
+list = [x for x in range(list_1, list_2 + 1)]
+permutations = permutation(list)
 print("Все перестановки", permutations)
