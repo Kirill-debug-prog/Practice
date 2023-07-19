ce215c8f688ae1627ed82d2c01705cfb843ee220
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 15:11:53 +1000

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,12 +1,13 @@
 import csv
 
-with open('books.csv', mode = 'r') as file:
+with open('books.csv', mode='r') as file:
     reader = csv.reader(file)
     next(reader)
     start_year = int(input("Введите начальный год: "))
     end_year = int(input("Введите конечный год: "))
     if start_year > end_year:
-        print("Некоректный запрос! Начальный год должен быть меньше или равен конечному году")
+        print("Некоректный запрос! "
+              "Начальный год должен быть меньше или равен конечному году")
     else:
         for row in reader:
             year = int(row[2])
