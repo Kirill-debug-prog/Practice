1452dd74e8594d2c3199c408812fd8aa8bee365a
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 16:14:12 +1000

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,9 +1,9 @@
-num=input("Введите число: ")
-n=len(num)
-sum=0
+num = input("Введите число: ")
+n = len(num)
+sum = 0
 for digit in num:
-    sum+=int(digit)**n
-if sum==int(num):
+    sum += int(digit) ** n
+if sum == int(num):
     print(num, "является числом Армстронга")
 else:
     print(num, "не является числом Армстронга")
