b9cf24863681db4c17ff2029681eaddf82b7191e
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 17:09:09 +1000

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
+from typing import Callable, Any
+
 num = [1, 3, 6, 8, 4]
-meaning = lambda nums: sum(nums) / len(nums)
+meaning: Callable[[Any], float | Any] = lambda nums: sum(nums) / len(nums)
 print(meaning(num))
