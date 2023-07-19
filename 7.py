1c6316744a368d3e11699e92d447905d5e6f4bdf
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 16:17:12 +1000

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,7 +1,7 @@
-total=0
+total = 0
 while True:
     num=(int(input("Введите  число")))
-    if num>0:
+    if num > 0:
         break
-    total+=num
+    total += num
     print("Сумма всех введенных чисел", total)
