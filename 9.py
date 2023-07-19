010544b48e2613c28dced16328b2331cef686b30
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 16:26:19 +1000

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,6 +1,6 @@
 num = (input("Введите натуральное число, в котором все цифры различны: "))
 max_digit = max(num)
-from_end = num[::-1].index(max_digit)+1
+from_end = num[::-1].index(max_digit) + 1
 from_start = num.index(max_digit)
 print("Порядковый номер максимальной цифры от конца числа", from_end)
 print("Порядковый номер максимальной цифры от начала числа", from_start)
