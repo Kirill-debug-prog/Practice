df1ca0cc519a45a62be2cbe119cacb67cfa27ba6
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 16:12:43 +1000

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,18 +1,17 @@
-import random
-colors=["красный", "синий", "черный", "зеленый", "желтый", "ораньжевый"]
-program_choice=random.choice(colors)
+import random = ["красный", "синий", "черный", "зеленый", "желтый", "ораньжевый"]
+program_choice = random.choice(colors)
 
 print("Выберите один из следующих цветов")
 for color in colors:
     print(color)
 
-user_choice=input("Ваш выбор: ")
-while user_choice!=program_choice:
+user_choice = input("Ваш выбор: ")
+while user_choice != program_choice:
     print("Не угадали! Попробуйте еще раз")
-    if colors.index(program_choice)>colors.index(user_choice):
+    if colors.index(program_choice) > colors.index(user_choice):
         print("Правильный цвет находится ближе к началу списка")
     else:
         print("Правильный ответ находится ближе к концу списка")
-    user_choice=input("Ваш выбор")
+    user_choice = input("Ваш выбор")
 
 print("Отлично! Вы угадали цвет.")
