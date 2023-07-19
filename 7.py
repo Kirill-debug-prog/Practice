ca2eec1393825413768243b466c12b1481394d28
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 14:53:14 +1000

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,10 +1,10 @@
-string=input("Вевиде строку: ")
-new_string=""
+string = input("Вевиде строку: ")
+new_string = ""
 for char in string:
     if char.islower():
-        new_string+=char.upper()
+        new_string += char.upper()
     elif char.isupper():
-        new_string+=char.lower()
+        new_string += char.lower()
     else:
-        new_string+=char
+        new_string += char
 print(new_string)
