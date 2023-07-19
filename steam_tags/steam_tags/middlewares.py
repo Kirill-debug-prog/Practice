dc10f526887138ce7e5a09e3d4368a04bd57e9f6
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-19 17:43:26 +1000

    Update middlewares.py

--- steam_tags/steam_tags/middlewares.py
+++ steam_tags/steam_tags/middlewares.py
@@ -3,28 +3,28 @@ from scrapy import signals
 
 class SteamTagsSpiderMiddleware:
     @classmethod
-    
     def from_crawler(cls, crawler):
         s = cls()
         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
         return s
-    
+
     def process_spider_input(self, response, spider):
         return None
-    
+
     def process_spider_output(self, response, result, spider):
         for i in result:
             yield i
- 
+
     def process_spider_exception(self, response, exception, spider):
         pass
-    
+
     def process_start_requests(self, start_requests, spider):
         for r in start_requests:
             yield r
-    
+
     def spider_opened(self, spider):
         spider.logger.info("Spider opened: %s" % spider.name)
+
 
 class SteamTagsDownloaderMiddleware:
     @classmethod
@@ -38,7 +38,7 @@ class SteamTagsDownloaderMiddleware:
 
     def process_response(self, request, response, spider):
         return response
-        
+
     def process_exception(self, request, exception, spider):
         pass
 
