439e3f5a5d628b91caefdb69aecf83af748291ab
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-24 11:50:39 +1000

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,4 @@
-from typing import Callable, Any, List
-
-fibonacci: Callable[[Any], list[int]] = \
-    lambda n: [0, 1] + [fibonacci(i - 1) + fibonacci(i - 2)
-                        for i in range(2, n + 1)
-                        if fibonacci(i - 1) + fibonacci(i - 2) <= n]
+fibonacci = lambda n: [0, 1] + [fibonacci(i-1) + fibonacci(i-2)
+                                for i in range(2, n+1)
+                                if fibonacci(i-1) + fibonacci(i-2) <= n]
 print(fibonacci(50))
