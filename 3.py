62639d4b69b590fd67b9dbc3d628df2842e11b0d
Kirill-debug-prog <137612667+Kirill-debug-prog@users.noreply.github.com>
2023-07-24 11:51:04 +1000

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,3 @@
-from typing import Callable, Any
-
-even: Callable[[Any], bool] = lambda n: True if n % 2 == 0 else False
+even = lambda n: True if n % 2 == 0 else False
 print(even(12))
 print(even(17))
